06796d3c765203ff7045f4f46b61972f3722b1fa
rndbblnn <waterseller@gmail.com>
2022-05-22 20:37:30 -0400

    improved scritps

--- queries/useful_queries.sql
+++ queries/useful_queries.sql
@@ -4,22 +4,25 @@ select * from group_metrics_dollar('2022-05-15', '2022-05-21') order by date des
 
 
 -- top 10 $ winners
-select * from trades
-where exit_time >= '2022-05-15' and exit_time <= '2022-05-21' 
+select symbol, trade_direction, exit_time, qty_total, pnl_dollar - fees_total as "pnl_dollar_inc_fees", pnl_percent
+from trades
+where exit_time >= '2022-05-15' and exit_time <= '2022-05-21'
 and pnl_dollar > 0
 order by pnl_dollar desc, exit_time desc
 limit 10;
 
 -- top 10 $ losers
-select * from trades
-where exit_time >= '2022-05-15' and exit_time <= '2022-05-21' 
+select symbol, trade_direction, exit_time, qty_total, pnl_dollar - fees_total as "pnl_dollar_inc_fees", pnl_percent
+from trades
+where exit_time >= '2022-05-15' and exit_time <= '2022-05-21'
 and pnl_dollar < 0
 order by pnl_dollar asc, exit_time desc
 limit 10;
 
 -- top 10 % losers
-select * from trades
-where exit_time >= '2022-05-15' and exit_time <= '2022-05-21' 
+select symbol, trade_direction, exit_time, qty_total, pnl_dollar - fees_total as "pnl_dollar_inc_fees", pnl_percent
+from trades
+where exit_time >= '2022-05-15' and exit_time <= '2022-05-21'
 and pnl_percent  < 0
 order by pnl_percent asc, exit_time desc
 limit 10;
